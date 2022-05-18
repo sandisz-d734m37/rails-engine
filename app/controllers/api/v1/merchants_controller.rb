1fb6f8897a6245441ef3292ce1e79ca11a94764c
Sandisz Thieme <sandiszthieme@gmail.com>
2022-05-18 12:04:42 -0600

    Create merchant/:id/items endpoint

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -4,4 +4,7 @@ class Api::V1::MerchantsController < ApplicationController
     render json: MerchantSerializer.new(Merchant.all)#, status: :ok
   end
 
+  def show
+    render json: MerchantSerializer.new(Merchant.find(params[:id]))#, status: :ok
+  end
 end
