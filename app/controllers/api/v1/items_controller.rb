6717152e11eb1f147d5bd518c7e9c75456cd60e7
Sandisz Thieme <sandiszthieme@gmail.com>
2022-05-18 12:32:39 -0600

    Create Item Show endpoint

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,14 @@
 class Api::V1::ItemsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
-    render json: ItemSerializer.new(@merchant.items)#, status: :ok
+    render json: ItemSerializer.new(@merchant.items)
+  end
+
+  # def index
+  #   render json: ItemSerializer.new(Item.all)
+  # end
+
+  def show
+    render json: ItemSerializer.new(Item.find(params[:id]))
   end
 end
