1557b1e14135d04e0b6a88216a0d87a6878f0feb
Sandisz Thieme <sandiszthieme@gmail.com>
2022-05-19 19:12:56 -0600

    Item find returns one item

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -18,4 +18,19 @@ class ItemSerializer
       end
     }
   end
+
+  def self.single_item(item)
+    {
+      "data":{
+        "id": item.id.to_s,
+        "type": 'item',
+        "attributes":{
+          "name": item.name,
+          "description": item.description,
+          "unit_price": item.unit_price,
+          "merchant_id": item.merchant_id
+        }
+      }
+    }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/api/v1/merchants/find", to: "api/v1/merchants#find"
   get "/api/v1/merchants/find_all", to: "api/v1/merchants#find_all"
+  get "/api/v1/items/find", to: "api/v1/items#find"
   get "/api/v1/items/find_all", to: "api/v1/items#find_all"
   get "/api/v1/merchants/:merchant_id/items", to: "api/v1/merchant_items#index"
   get "/api/v1/items/:item_id/merchant", to: "api/v1/merchant_items#show"
