1f6df42f49a3442ae43456afc17148ea216af95b
Sandisz Thieme <sandiszthieme@gmail.com>
2022-05-19 23:10:24 -0600

    Refactor: merchant_item controller fails gracefully

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -1,11 +1,25 @@
 class Api::V1::MerchantItemsController < ApplicationController
   def index
-    @merchant = Merchant.find(params[:merchant_id])
-    render json: ItemSerializer.new(@merchant.items)
+    if Merchant.where(id: params[:merchant_id]).empty?
+      render json: {
+          error: "No such merchant; check the entered ID",
+          status: 404
+        }, status: 404
+    else
+      @merchant = Merchant.find(params[:merchant_id])
+      render json: ItemSerializer.new(@merchant.items)
+    end
   end
 
   def show
-    @item = Item.find(params[:item_id])
-    render json: MerchantSerializer.new(@item.merchant)
+    if Item.where(id: params[:item_id]).empty?
+      render json: {
+          error: "No such item; check the entered ID",
+          status: 404
+        }, status: 404
+    else
+      @item = Item.find(params[:item_id])
+      render json: MerchantSerializer.new(@item.merchant)
+    end
   end
 end
