e27723740fb9cdcc6da25f3ae8e230086b38d966
Sandisz Thieme <sandiszthieme@gmail.com>
2022-05-26 18:23:24 -0600

    create Invoice#total_sales_for class method

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -6,4 +6,9 @@ class Invoice < ApplicationRecord
   has_many :invoice_items
 
   has_many :items, through: :invoice_items
+
+  def self.total_sales_for(start_date, end_date)
+    select('SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue').joins(:transactions, :invoice_items).where("transactions.result = 'success' AND invoices.status = 'shipped' AND invoice_items.created_at BETWEEN '#{start_date} 00:00:00' AND '#{end_date} 00:00:00'")
+    # select('SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue').joins(:transactions, :invoice_items).where("transactions.result = 'success' AND invoices.status = 'shipped' AND invoice_items.created_at BETWEEN '2000-03-09 00:00:00' AND '2022-03-24 00:00:00'")
+  end
 end
--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -3,7 +3,11 @@ class InvoiceItem < ApplicationRecord
   belongs_to :item
 
   has_one :merchant, through: :item
-  
+
   has_many :transactions, through: :invoice
   has_many :customers, through: :invoice
+
+  def self.total_sales_for(start_date, end_date)
+    select(`SUM(invoice_items.quantity * invoice_items.unit_price) as revenue`).joins(invoices: :transactions).where("transactions.result = success AND incoives.status = shipped AND invoice_items.created_at BETWEEN ``#{start_date} 00:00:00` AND invoice_items.updated_at > ``#{end_date} 00:00:00").group(:id)
+  end
 end
