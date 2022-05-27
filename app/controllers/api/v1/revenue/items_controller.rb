d5d07ea3e8b2b760ca4fc54c1eda0db50cff6c49
Sandisz Thieme <sandiszthieme@gmail.com>
2022-05-26 21:13:53 -0600

    Top items by revenue endpoint complete

--- app/controllers/api/v1/revenue/items_controller.rb
+++ app/controllers/api/v1/revenue/items_controller.rb
@@ -1,4 +1,15 @@
 class Api::V1::Revenue::ItemsController < ApplicationController
  def index
+   if params[:quantity].nil?
+     items = Item.top_items_by_revenue(10)
+     render json: ItemRevenueSerializer.new(items)
+   elsif params[:quantity].to_i > 0
+     items = Item.top_items_by_revenue(params[:quantity])
+     render json: ItemRevenueSerializer.new(items)
+   else
+     render json: {
+       data: { message: 'Error' }
+     }, status: 400
+   end
  end
 end
