596e9f0882d31a1ad1981a1bac2212378934a1a6
Sandisz Thieme <sandiszthieme@gmail.com>
2022-05-26 21:13:38 -0600

    Create Item#top_items_by_revenue method

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,4 +10,13 @@ class Item < ApplicationRecord
   def self.search(name)
     where('name ILIKE ?', "%#{name.downcase}%").order(:name)
   end
+
+  def self.top_items_by_revenue(quantity = 10)
+    select("items.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue")
+    .joins(invoice_items: [:invoice, :transactions])
+    .group(:id)
+    .where(transactions: {result: "success"}, invoices: {status: "shipped"})
+    .order(revenue: :desc)
+    .limit(quantity)
+  end
 end
