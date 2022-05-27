fa31d841c56b0da79415cb703d7d7e185464a072
Sandisz Thieme <sandiszthieme@gmail.com>
2022-05-26 21:13:08 -0600

    Create ItemRevenueSerializer

--- app/serializers/item_revenue_serializer.rb
+++ app/serializers/item_revenue_serializer.rb
@@ -1,4 +1,8 @@
 class ItemRevenueSerializer
   include JSONAPI::Serializer
-  attributes 
+  attributes :name, :description, :unit_price, :merchant_id
+
+  attributes :revenue do |item|
+    item.revenue
+  end
 end
