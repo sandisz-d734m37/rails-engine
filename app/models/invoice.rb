fc5477b74964b3cd867a995e0574f5179e703937
Sandisz Thieme <sandiszthieme@gmail.com>
2022-05-26 21:28:40 -0600

    Creat Invoice#unshipped_revenue method

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -13,4 +13,13 @@ class Invoice < ApplicationRecord
     .sum('(invoice_items.quantity * invoice_items.unit_price)')
     # select('SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue').joins(:transactions, :invoice_items).where("transactions.result = 'success' AND invoices.status = 'shipped' AND invoice_items.created_at BETWEEN '2000-03-09 00:00:00' AND '2022-03-24 00:00:00'")
   end
+
+  def self.unshipped_revenue(quantity = 10)
+    select('invoices.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS potential_revenue')
+    .joins(:transactions, :invoice_items)
+    .group(:id)
+    .where("transactions.result = 'success' AND invoices.status != 'shipped'")
+    .order(potential_revenue: :desc)
+    .limit(quantity)
+  end
 end
