946172d74f8c36f73b4ad1a9c10a44855d34f8f0
Sandisz Thieme <sandiszthieme@gmail.com>
2022-05-26 21:32:23 -0600

    Add index action to Unshipped Controller

--- app/controllers/api/v1/revenue/unshipped_controller.rb
+++ app/controllers/api/v1/revenue/unshipped_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::Revenue::UnshippedController
   def index
+    invoices = Invoice.unshipped_revenue(params[:quantity])
+    render json: UnshippedOrderSerializer.new(invoices)
   end
 end
